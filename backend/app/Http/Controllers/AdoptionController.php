725b2a94e3c1837cb6e7379c2d0884a3fff7d4d8
jotahtsx <joaomanoelskai@gmail.com>
2022-06-06 23:43:44 -0300

    Criando regra de adoção por único email

--- backend/app/Http/Controllers/AdoptionController.php
+++ backend/app/Http/Controllers/AdoptionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Rules\OnlyPetAdoption;
 use App\Http\Resources\AdoptionCollection;
 use App\Models\Adoption;
 use Illuminate\Http\Request;
@@ -19,7 +20,7 @@ class AdoptionController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            "email" => ['required', 'email'],
+            "email" => ['required', 'email', new OnlyPetAdoption()],
             "allowance_amount" => ['required', 'numeric', 'between:10,100'],
             "pet_id" => ['required', 'int', 'exists:pets,id']
         ]);
