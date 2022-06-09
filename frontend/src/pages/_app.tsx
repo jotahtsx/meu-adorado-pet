993335c06a255ee43020bef237ec15f2c4177646
jotahtsx <joaomanoelskai@gmail.com>
2022-06-09 06:20:32 -0300

    Finalizando breakpoints do footer

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,13 +2,13 @@ import '../ui/styles/globals.css'
 import type { AppProps } from 'next/app'
 import {ThemeProvider} from '@mui/material'
 import theme from '../ui/themes/theme'
-import Header from '../ui/components/Header/Header'
 import Footer from '../ui/components/Footer/Footer'
+import HeaderAdmin from '../ui/components/HeaderAdmin/HeaderAdmin'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
+      <HeaderAdmin />
       <Component {...pageProps} />
       <Footer />
     </ThemeProvider>
